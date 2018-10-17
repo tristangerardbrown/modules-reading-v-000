e6a932fd7da367bbfa49b1182d80cccc51fceec5
Tristan Brown <tgbrown@stanford.edu>
2018-10-17 14:42:52 +0000

    automatically backed up by learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,11 +1,6 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
-
 class Dancer
-  include Dance
-
-  attr_accessor :name
-  def initialize(name)
-    @name = name
+  extend MetaDancing
   end
-end
+  
