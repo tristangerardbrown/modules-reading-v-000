39c026af132e86d33fa4740356035497fbddc8e5
Tristan Brown <tgbrown@stanford.edu>
2018-10-17 14:55:48 +0000

    automatically backed up by learn

--- lib/fancy_dance.rb
+++ lib/fancy_dance.rb
@@ -4,3 +4,10 @@ module FancyDance
       "I'm just one jumping instance"
     end
   end
+
+  module ClassMethods                 # you "extend" these class methods
+    def megajump 
+      "I'm a whole class of MOFO's jumping"
+    end
+  end 
+end
